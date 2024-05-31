c04fd7607bec2438e0512462fb15b9694ec31a83
Edward Ingram <edward.ingram23@gmail.com>
2024-05-31 14:00:32 -0700

    update script

--- install_col_repos.sh
+++ install_col_repos.sh
@@ -1,16 +1,16 @@
 # Run in code/ansible
 
-mkdir ~/code/ansible/collections/ansible_collections/eingram23
-ansible-galaxy collection install -r requirements.yml -p ~/code/ansible/collections
+mkdir -p ~/code/ansible/collections/ansible_collections/eingram23
+ansible-galaxy collection install -r requirements.yml -p ~/code/ansible/collections --force
 cd ~/code/ansible/collections/ansible_collections/eingram23
 
-git clone git@github.com:eingram23/ansible-col-homelab.git ./homelab
-git clone git@github.com:eingram23/ansible-col-containers.git ./containers
-git clone git@github.com:eingram23/ansible-col-tf_build.git ./tf_build
-git clone git@github.com:eingram23/ansible-col-database.git ./database
-git clone git@github.com:eingram23/ansible-col-kubernetes.git ./kubernetes
-git clone git@github.com:eingram23/ansible-col-pihole.git ./pihole
-git clone git@github.com:eingram23/ansible-col-vsphere.git ./vsphere
-git clone git@github.com:eingram23/ansible-col-splunk.git ./splunk
-git clone git@github.com:eingram23/ansible-col-gcp.git ./gcp
-git clone git@github.com:eingram23/ansible-col-monitoring.git ./monitoring
+git clone git@github.com:eingram23/ansible-col-homelab.git ~/code/ansible/collections/ansible_collections/eingram23/homelab
+git clone git@github.com:eingram23/ansible-col-containers.git ~/code/ansible/collections/ansible_collections/eingram23/containers
+git clone git@github.com:eingram23/ansible-col-tf_build.git ~/code/ansible/collections/ansible_collections/eingram23/tf_build
+git clone git@github.com:eingram23/ansible-col-database.git ~/code/ansible/collections/ansible_collections/eingram23/database
+git clone git@github.com:eingram23/ansible-col-kubernetes.git ~/code/ansible/collections/ansible_collections/eingram23/kubernetes
+git clone git@github.com:eingram23/ansible-col-pihole.git ~/code/ansible/collections/ansible_collections/eingram23/pihole
+git clone git@github.com:eingram23/ansible-col-vsphere.git ~/code/ansible/collections/ansible_collections/eingram23/vsphere
+git clone git@github.com:eingram23/ansible-col-splunk.git ~/code/ansible/collections/ansible_collections/eingram23/splunk
+git clone git@github.com:eingram23/ansible-col-gcp.git ~/code/ansible/collections/ansible_collections/eingram23/gcp
+git clone git@github.com:eingram23/ansible-col-monitoring.git ~/code/ansible/collections/ansible_collections/eingram23/monitoring
